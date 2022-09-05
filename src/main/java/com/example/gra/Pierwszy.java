1936a672a0936b89a0233b9ea7eec1047df1aac6
Jakubk20 <108329174+Jakubk20@users.noreply.github.com>
2022-09-05 21:52:46 +0200

    Poprawiki z money

--- src/main/java/com/example/gra/Pierwszy.java
+++ src/main/java/com/example/gra/Pierwszy.java
@@ -4,19 +4,23 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
-public class Pierwszy extends Geralt{
+public class Pierwszy{
 
     @FXML
     private Label silla;
     @FXML
     private Label ballans;
+    @FXML
+    private Label zdrowie;
 
 
 
     @FXML
     protected  void onPokaz(){
-        ballans.setText(String.valueOf(money));
-        silla.setText(String.valueOf(power));
+        ballans.setText(String.valueOf(Geralt.money));
+        silla.setText(String.valueOf(Geralt.power));
+        zdrowie.setText(String.valueOf(Geralt.HP));
+
 
     }
 
--- src/main/java/com/example/gra/WalkaZBiesem.java
+++ src/main/java/com/example/gra/WalkaZBiesem.java
@@ -1,4 +1,21 @@
 package com.example.gra;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
 public class WalkaZBiesem {
+    @FXML
+    private Label sila;
+    @FXML
+    private Label balans;
+    @FXML
+    private Label hapeki;
+    @FXML
+    protected  void onPokazz(){
+        balans.setText(String.valueOf(Geralt.money));
+        sila.setText(String.valueOf(Geralt.power));
+        hapeki.setText(String.valueOf(Geralt.HP));
+
+
+    }
 }
