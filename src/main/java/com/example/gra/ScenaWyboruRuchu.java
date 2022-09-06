241446b7e0387cfdbf4c36558d09b463936caad0
Jakubk20 <108329174+Jakubk20@users.noreply.github.com>
2022-09-06 19:32:32 +0200

    Dzień 2.2 (Handlarz fxml)(poprawka)(poprawka poprawki)

--- src/main/java/com/example/gra/ScenaWyboruRuchu.java
+++ src/main/java/com/example/gra/ScenaWyboruRuchu.java
@@ -1,4 +1,11 @@
 package com.example.gra;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
 public class ScenaWyboruRuchu {
+    @FXML
+    protected void onHandlarz(ActionEvent actionEvent){
+
+    }
 }
