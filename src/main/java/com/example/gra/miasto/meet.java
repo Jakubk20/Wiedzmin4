cac786f513e939a16989c542b6063d374ae54427
PiecdziesiatkaTrygonometryczna <dawidkalinowski243@gmail.com>
2022-09-11 23:29:48 +0200

    rozpoczęcie II części Taska Jaskiera & naprawione wszystkie sceny

--- src/main/java/com/example/gra/miasto/meet.java
+++ src/main/java/com/example/gra/miasto/meet.java
@@ -58,21 +58,14 @@ public class meet {
 
     @FXML
     protected void onTavern(ActionEvent actionEvent) throws IOException {
-        if (banditTookMoney){
+        if (banditTookMoney || jaskierQuestTaken) {
             FXMLLoader fxmlLoader = new FXMLLoader(start.class.getResource("jaskierKarczma/banditTookMoney.fxml"));
             Scene scene = new Scene(fxmlLoader.load());
             Stage stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
             stage.setScene(scene);
             stage.centerOnScreen();
             stage.show();
-        } else if (jaskierQuestTaken){
-            FXMLLoader fxmlLoader = new FXMLLoader(start.class.getResource("jaskierKarczma/tutrzebazmienic.fxml")); // tu trzeba zmienic
-            Scene scene = new Scene(fxmlLoader.load());
-            Stage stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
-            stage.setScene(scene);
-            stage.centerOnScreen();
-            stage.show();
-        } else if (jaskierQuest1Done){
+        } else if (jaskierQuest1Done) {
             FXMLLoader fxmlLoader = new FXMLLoader(start.class.getResource("jaskierKarczma/jaskierNone.fxml"));
             Scene scene = new Scene(fxmlLoader.load());
             Stage stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
@@ -89,6 +82,7 @@ public class meet {
         }
 
     }
+
     @FXML
     protected void onGoNorthClick(ActionEvent actionEvent) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(start.class.getResource("novigrad/port.fxml"));
@@ -98,6 +92,7 @@ public class meet {
         stage.centerOnScreen();
         stage.show();
     }
+
     @FXML
     protected void onGoSouthClick(ActionEvent actionEvent) throws IOException {
         if (isZygfryd) {
